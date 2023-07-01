8b4229fc3f181ecdd2aa1f6cddafe99dcfe9f219
John Yevsukov <johnyevsukov@Johns-MacBook-Pro.local>
2023-06-30 21:21:38 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Game } from "./components/pages/Game/Game";
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Game/Game.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as styles from "./styles";
+
+export const Game = () => {
+  return <styles.Wrapper>Trivia</styles.Wrapper>;
+};
